afa809eb7988642e5c5457f8146194b3c0e30f28
Agustin <equite.augusto@gmail.com>
2022-11-08 10:43:34 +0300

    required express,listening to port 3000, sending message to browser

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('hello from yelp camp')
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
