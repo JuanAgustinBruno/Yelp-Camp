c50c961dcfe1eec927d98c5286f344ccf29b05f0
Agustin <equite.augusto@gmail.com>
2022-11-08 11:22:52 +0300

    set template engine, path to /views/..,render home.ejs

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express');
 
+//require express to run the server
+const express = require('express');
 const app = express();
 
+//set the template engine to use
+app.set('view engine', 'ejs');
+
+//sets the path to views folder
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'))
+
+//request to localhost:3000/
 
 app.get('/', (req, res) => {
-    res.send('hello from yelp camp')
+    res.render("home")
 });
 
 
+//set port listening
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
