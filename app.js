78d17bc28467b78af47a72e5271c5d61f3a588cc
Agustin <equite.augusto@gmail.com>
2022-11-08 12:34:44 +0300

    required mongoose, connected to db, added logic on error, added msg on open connection

--- app.js
+++ app.js
@@ -3,6 +3,29 @@
 const express = require('express');
 const app = express();
 
+//require mongoose 
+const mongoose = require('mongoose');
+
+//connect to data base
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    //useCreateIndex: true, (not supported by Mongoose)
+    //https://www.mongodb.com/community/forums/t/option-usecreateindex-is-not-supported/123048/4
+    useUnifiedTopology: true
+});
+
+//db set as shortcut of mongoose.connection
+const db = mongoose.connection;
+
+//logic to check if there is an error
+db.on("error", console.error.bind(console, "connection error:"));
+
+//logic for succesfull connection
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
 //set the template engine to use
 app.set('view engine', 'ejs');
 
@@ -10,10 +33,11 @@ app.set('view engine', 'ejs');
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'))
 
+
 //request to localhost:3000/
 
 app.get('/', (req, res) => {
-    res.render("home")
+    res.render("home") // "home" stands for home.ejs, not just a string
 });
 
 
