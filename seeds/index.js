aa91d24954569a9ec85e5ababe607b3044a3dada
Agustin <equite.augusto@gmail.com>
2022-11-08 20:20:46 +0300

    Imported cities.js and seedHelpers.js, created function to take a random sample from an array, created a function that deletes everything from the db and extract 50 random cities from cities.js asign them random title and save them to the db

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,11 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground'); // modified:   ".." <- this 2 dots mean have to go 2 steps to the proper directory(or something like that)
 
+//Import from cities.js
+const cities = require('./cities'); 
+//import from seedHelpers.js
+const { places, descriptors } = require('./seedHelpers');
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     //useCreateIndex: true,
@@ -15,13 +20,25 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-//function to delete everything from database collection , create new entry and save.
+
+//takes a random sample from an array
+
+const sample = array => array[Math.floor(Math.random() * array.length)]; 
+
+//Function to randomize 50 cities and save them with random title on the db
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({ title: "purple field"});
-    await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000); 
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`, 
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
 }
-//execute the function
+
+/* //execute the function */
 
 seedDB();
