32ef53f51c1eecbbedee2d0617d0000187be351a
Agustin <equite.augusto@gmail.com>
2022-12-23 17:52:03 +0300

    required route to users router, modified variable name of other routes

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const methodOverride = require('method-override');
 const passport = require('passport'); 
 const LocalStrategy = require('passport-local'); 
 const User = require('./models/user');
+const userRoutes = require('./routes/users');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -64,8 +65,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
