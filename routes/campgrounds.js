4d07c99f8e40ba703ac823f65f5109f66ff19fe3
Agustin <equite.augusto@gmail.com>
2023-01-22 20:27:50 +0300

    upload images on new campground using cloudinary(.env file must be created with credentials or wont work, i didnt added to avoid abuses)

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,28 @@
+
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-const Campground = require('../models/campground');
 const campgrounds = require('../controllers/campgrounds');
+const {storage} = require('../cloudinary');
+const multer  = require('multer')
+const upload = multer({ storage })
 
-router.get('/', catchAsync(campgrounds.index));
+const Campground = require('../models/campground');
+
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get( catchAsync(campgrounds.showCampground))
+    .put( isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete( isLoggedIn, isAuthor,  catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor,  catchAsync(campgrounds.deleteCampground));
-
 
 module.exports = router;
